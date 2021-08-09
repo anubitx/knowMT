fab1b4cfa33dbeecbd383aef077f479261751397
Simarpreet <bhurjirashi@gmail.com>
2021-08-09 14:00:04 +1000

    Updated files for all steps

--- main.py
+++ main.py
@@ -1,45 +1,48 @@
 from Numt_Tag import numt_chrM_overlapping_reads
-# from var_ovlp import remove_mitochondrial_noise
+from var_ovlp import remove_mitochondrial_noise
+from results import get_result
 # from tree import contigs_flanking_numts
 # from kmer_class import kmer_approach
+from alignment_approach import alignment_approach
+from cluster import cluster_contigs
 import hardcoded_path
+import subprocess
+import os 
 import argparse
-import shutil, os
-
-def copy_bam_files(bam_folderpath):
-    destination = hardcoded_path.bam_dir
-    for i in os.listdir(bam_folderpath):
-        shutil.copy(i, destination)  
-    # print(os.listdir(destination))
-        # print(i)
+import pandas as pd
 
 if __name__== "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-max-ksize", default=35, type=int)
-    parser.add_argument("-threads", default=1, type=int)
-    parser.add_argument("-min-ksize", default=17, type=int)
-    parser.add_argument("-numtbed", default=None, type=str)
-    parser.add_argument("-inputbam", default=None, type=str)
-    parser.add_argument("-outputdir", default=None, type=str)
-    parser.add_argument("-reference", default=None, type=str)
-    args = parser.parse_args()
-    # if args.bam_folderpath is not None:
-    #     copy_bam_files(args.bam_folderpath)
-    # if args.bed_filename is not None:
-    #     shutil.copy(args.bed_filename, hardcoded_path.bed_dir) 
-
-    a = numt_chrM_overlapping_reads(args.numtbed, args.inputbam, args.outputdir, args.threads, args.reference)
-    # a = numt_chrM_overlapping_reads(args.bed_filename, args.bam_folderpath)
-    # copy_bam_files("original_files/Mt_input_bam")
-    # a = numt_chrM_overlapping_reads(args.bed_file_name, args.bam_file_path)
-    
-    # a.step3_for_all_files()
-    # a = numt_chrM_overlapping_reads.from_reads_mapped_to_Mt(hardcoded_path.bed_dir, hardcoded_path.Mt_input_bam)
-    # # a.generate_numt_tag()
-    # c = remove_mitochondrial_noise(hardcoded_path.step3_output_dir)
-    # c.get_insert_stats()
+    # parser.add_argument("-max-ksize", default=35, type=int)
+    # parser.add_argument("-min-ksize", default=17, type=int)
+    # args = parser.parse_args()
+    # n = numt_chrM_overlapping_reads(hardcoded_path.bed_dir, hardcoded_path.bam_dir)
+    # n.step3_for_all_files()
+    c = remove_mitochondrial_noise()
+    c.step3_for_all_files()
+    c.get_insert_stats()
+    # ref_mmi_cmd = "minimap2 -I 500M -d /home/simarpreet/Desktop/Remote_pc_files/ref.mmi /home/simarpreet/Desktop/Remote_pc_files/original_files/Reference_Genome_GRCh38.p13/GRCh38.p13.alt.decoyhs38d1.hla.phix.sequin.patches.xy.fa"
+    # a = subprocess.run([ref_mmi_cmd], shell = True)
+    # out, err = a.communicate()
     # k = kmer_approach(args.max_ksize, args.min_ksize)
+    # k = kmer_approach(21,17)
     # k.generate_kmers()
     # k.assemble_masked_reads()
+    aln_app = alignment_approach()
+    aln_app.write_fastq_files(35)
+    aln_app.assemble_masked_reads()
     # flank = contigs_flanking_numts()
-    # flank.get_flanking_contigs()    
+    # flank.get_flanking_contigs()  
+    cluster = cluster_contigs()
+    cluster.get_clustered_contigs()  
+    result = get_result()
+    result.num_reads_ovlp_MT_nmts()
+    result.get_df_from_pickle()
+    result.get_result_df()
+    print(result.get_all_numts_for_all_samples())
+
+    ##call results.py later on 
+    
+# a = numt_chrM_overlapping_reads.from_reads_mapped_to_Mt(hardcoded_path.bed_dir, hardcoded_path.Mt_input_bam)
+
+
